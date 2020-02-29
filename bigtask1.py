3ad7822094bfc78a32577153569186a4a3b64c8e
Niipler <57757247+Niipler@users.noreply.github.com>
2020-02-29 20:51:28 +0300

    вроде добавил но стало хуже
    кароч я там кнопку смены мода добавил, теперь там все очень медленное

--- bigtask1.py
+++ bigtask1.py
@@ -3,7 +3,7 @@ import sys
 import pygame
 import requests
 LAN_STEP = 0.008
-LON_STEP = 0.02
+LON_STEP = 0.008
 coord_to_get_x = 0.0000428
 coord_to_get_y = 0.0000428
 map_file = "map.png"
@@ -14,7 +14,8 @@ class Mapa:
         self.lon = lon
         self.lan = lan
         self.zoom = zoom
-        self.type = "map"
+        self.map_type = "map"
+        self.i = 1
 
     def change_zoom(self, key):
         if key:
@@ -36,10 +37,21 @@ class Mapa:
         if self.lon > 180: self.lon -= 360
         if self.lon < -180: self.lon += 360
 
+    def change_type(self):
+        if self.i == 1:
+            self.map_type = 'sat'
+            self.i += 1
+        elif self.i == 2:
+            self.map_type = 'skl'
+            self.i += 1
+        elif self.i == 3:
+            self.map_type = 'map'
+            self.i = 1
+
 
 def load_map(mapa):
     map_request = "http://static-maps.yandex.ru/1.x/?ll={lan},{lon}&l={type}&z={zoom}".format\
-        (lan=mapa.lan, lon=mapa.lon, type=mapa.type, zoom=mapa.zoom)
+        (lan=mapa.lan, lon=mapa.lon, type=mapa.map_type, zoom=mapa.zoom)
     response = requests.get(map_request)
     if not response:
         print("Ошибка выполнения запроса:")
@@ -55,30 +67,33 @@ def main():
     screen = pygame.display.set_mode((600, 450))
     pygame.display.flip()
     mapa = Mapa()
-
-    while pygame.event.wait().type != pygame.QUIT:
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                break
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                x, y = event.pos
+                if x >= 500 and x <= 600 and y >= 400 and y <= 450:
+                    mapa.change_type()
+            key = pygame.key.get_pressed()
+            if key[pygame.K_PAGEUP]:
+                mapa.change_zoom(True)
+            if key[pygame.K_PAGEDOWN]:
+                mapa.change_zoom(False)
+            if key[pygame.K_UP]:
+                mapa.move_map('up')
+            if key[pygame.K_DOWN]:
+                mapa.move_map('down')
+            if key[pygame.K_LEFT]:
+                mapa.move_map('left')
+            if key[pygame.K_RIGHT]:
+                mapa.move_map('right')
         load_map(mapa)
         screen.blit(pygame.image.load(map_file), (0, 0))
+        pygame.draw.rect(screen, pygame.Color(255, 0, 0), (500, 400, 100, 50))
         pygame.display.flip()
-        key = pygame.key.get_pressed()
-        if key[pygame.K_PAGEUP]:
-            mapa.change_zoom(True)
-        if key[pygame.K_PAGEDOWN]:
-            mapa.change_zoom(False)
-        if key[pygame.K_UP]:
-            mapa.move_map('up')
-        if key[pygame.K_DOWN]:
-            mapa.move_map('down')
-        if key[pygame.K_LEFT]:
-            mapa.move_map('left')
-        if key[pygame.K_RIGHT]:
-            mapa.move_map('right')
-
     pygame.quit()
 
 
 if __name__ == "__main__":
     main()
-
-
-
